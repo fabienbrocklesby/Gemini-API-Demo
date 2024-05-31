8eb5a83b2f7e7381b71bb377272ada6a18cabf23
Fabien Brocklesby <fabienbrocklesby@icloud.com>
2024-05-31 13:13:40 +1200

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+dotenv.config();
+
+const app = new Hono();
+
+const port = process.env.PORT || 3000;
+const api_key = process.env.API_KEY;
+
+const genAI = new GoogleGenerativeAI(api_key);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+app.post("/", async (c) => {
+	const { prompt } = await c.req.parseBody();
+
+	return c.text((await model.generateContent(prompt)).response.text());
+});
+
+serve({
+	fetch: app.fetch,
+	port: port,
+});
+
+console.log("Server is running on port 3000 🚀");
